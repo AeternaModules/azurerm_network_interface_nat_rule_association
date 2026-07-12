c1e6bcf8a7a05fbfb6f5c36b01c84ea30ead1770
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "network_interface_nat_rule_associations_id" {
+  description = "Map of id values across all network_interface_nat_rule_associations, keyed the same as var.network_interface_nat_rule_associations"
+  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.id }
+}
 output "network_interface_nat_rule_associations_ip_configuration_name" {
   description = "Map of ip_configuration_name values across all network_interface_nat_rule_associations, keyed the same as var.network_interface_nat_rule_associations"
   value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.ip_configuration_name }
