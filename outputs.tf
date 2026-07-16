90f4e87d07f7f56e2004e7b4067739790c84db38
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "network_interface_nat_rule_associations_id" {
   description = "Map of id values across all network_interface_nat_rule_associations, keyed the same as var.network_interface_nat_rule_associations"
-  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.id }
+  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "network_interface_nat_rule_associations_ip_configuration_name" {
   description = "Map of ip_configuration_name values across all network_interface_nat_rule_associations, keyed the same as var.network_interface_nat_rule_associations"
-  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.ip_configuration_name }
+  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.ip_configuration_name if v.ip_configuration_name != null && length(v.ip_configuration_name) > 0 }
 }
 output "network_interface_nat_rule_associations_nat_rule_id" {
   description = "Map of nat_rule_id values across all network_interface_nat_rule_associations, keyed the same as var.network_interface_nat_rule_associations"
-  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.nat_rule_id }
+  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.nat_rule_id if v.nat_rule_id != null && length(v.nat_rule_id) > 0 }
 }
 output "network_interface_nat_rule_associations_network_interface_id" {
   description = "Map of network_interface_id values across all network_interface_nat_rule_associations, keyed the same as var.network_interface_nat_rule_associations"
-  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.network_interface_id }
+  value       = { for k, v in azurerm_network_interface_nat_rule_association.network_interface_nat_rule_associations : k => v.network_interface_id if v.network_interface_id != null && length(v.network_interface_id) > 0 }
 }
 
